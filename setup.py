a20d70f49ad9da98160309ccee30d1b7430a36d4
Adrian Del Maestro <adrian@delmaestro.org>
2020-05-07 09:38:17 -0400

    ready to distribute on PyPI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setuptools.setup(
     name='heprops',
-    version='0.2',
+    version='0.3',
     packages=setuptools.find_packages(),
     license='MIT',
     description='Properties of the chemical element helium.',
